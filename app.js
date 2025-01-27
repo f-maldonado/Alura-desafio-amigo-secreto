f2c4463932065209d3db2ebe42566f00c9411c24
Fernando Maldonado <f_maldonado@live.com.ar>
2025-01-27 04:52:30 -0300

    Se agrega funcion listarAmigos()

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
 let amigos = [];
 
 function agregarAmigo(){
+    // Captura del valor en input
     let ingresoAmigo = document.getElementById('amigo').value;
+    
+    // comprobacion de que no es un valor vacio
     if (ingresoAmigo == ''){
         alert('Por favor, inserte un nombre.')
     } else {
-        console.log(ingresoAmigo);
+        // agregar valor (nombre) a la lista
         amigos.push(ingresoAmigo);
-        //console.log(typeof(ingresoAmigo));
-        console.log(amigos);
+        // funcion que agrega items con los nombres en HTML
+        listarAmigos(amigos)
         // limpiar caja input
         document.getElementById('amigo').value = '';
     }
     return
 }
 
-//agregarAmigo();
+function listarAmigos(lista){
+    // captura del elemento lista ul
+    let listaAmigos = document.getElementById('listaAmigos')
+    // se vacia al iniciar (impide que queden valores anteriores)
+    listaAmigos.innerHTML = ''
+    let nombreAmigo = ''
+    
+    for(let i = 0; i < lista.length; i++){
+        // se recorre la lista (arreglo principal)
+        nombreAmigo = lista[i];
+        // se crea los elementos items li
+        let items = document.createElement('li')
+        // se asigna valor a los elementos
+        items.innerHTML = nombreAmigo
+        // se agrega los elementos al HTML 
+        listaAmigos.append(items)
+    }
+    return
+}
